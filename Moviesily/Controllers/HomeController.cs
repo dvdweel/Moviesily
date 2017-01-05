6c0ea87bc0a9ff7edf54d97169ed6a6983c99700
dvdweel <deborah.vanderweel@gmail.com>
2017-01-05 15:26:45 +0100

    Add data for movie database but not working yet.

---
+++ Moviesily/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Moviesily.Models;
+using Moviesily.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Moviesily.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using (var db = new MovieContext())
+            {
+                var HomeVM = new HomeVM();
+                HomeVM.Genres = db.Genres.ToList();
+                return View(HomeVM);
+            }
+        }
+    }
+}
---
+++ Moviesily/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Moviesily.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext() : base("Moviesily")
+        { }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
